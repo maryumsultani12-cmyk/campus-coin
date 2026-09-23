d7fe55864944d944eed4876264f2baa186d2f8bc
maryam <maryumsultani12@gmail.com>
2026-09-23 19:37:35 +0500

    admin panel

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,43 @@
 
 use Illuminate\Support\Facades\Route;
 
+
+//ADNIN ROUTES
 Route::get('/', function () {
-    return view('welcome');
+    return view('admin-panel.index');
 });
 
+Route::get('/admin-panel/calendar' , function () {
+    return view('.admin-panel.calendar');
+});
+
+Route::get('/admin-panel/forms' , function () {
+    return view('admin-panel.forms');
+});
+
+Route::get('/admin-panel/icons' , function () {
+    return view('admin-panel.icons');
+});
+
+Route::get('/admin-panel/login' , function () {
+    return view('admin-panel.login');
+});
+
+Route::get('/admin-panel/profile' , function () {
+    return view('admin-panel.profile');
+});
+
+Route::get('/admin-panel/register' , function () {
+    return view('admin-panel.register');
+});
+
+Route::get('/admin-panel/reset-password' , function () {
+    return view('admin-panel.reset-password');
+});
+
+Route::get('/admin-panel/tables' , function () {
+    return view('admin-panel.tables');
+});
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
